0f7c69077103daf668897b6f3653884b725f3f09
Felix Häusler <hfelix@protonmail.ch>
2018-04-03 17:58:09 +0200

    enable frameless window on Linux

--- src/main/createWindow.js
+++ src/main/createWindow.js
@@ -2,7 +2,7 @@
 
 import fs from 'fs'
 import path from 'path'
-import { BrowserWindow, app } from 'electron'
+import { BrowserWindow, app, screen } from 'electron'
 import windowStateKeeper from 'electron-window-state'
 import { isMarkdownFile } from './utils'
 
@@ -15,7 +15,7 @@ const createWindow = (pathname, options = {}) => {
     defaultHeight: 800
   })
 
-  const { x, y, width, height } = mainWindowState
+  const { x, y, width, height } = ensureWindowPosition(mainWindowState)
   const winOpt = Object.assign({ x, y, width, height }, {
     icon: path.join(__static, 'logo-96px.png'),
     minWidth: 450,
@@ -25,10 +25,11 @@ const createWindow = (pathname, options = {}) => {
     },
     useContentSize: true,
     show: false,
-    frame: process.platform === 'linux',
+    frame: false,
     titleBarStyle: 'hidden'
   }, options)
   let win = new BrowserWindow(winOpt)
+  mainWindowState.manage(win)
 
   const winURL = process.env.NODE_ENV === 'development'
     ? `http://localhost:9080`
@@ -68,6 +69,31 @@ const createWindow = (pathname, options = {}) => {
 
   windows.set(win.id, win)
   return win
+}
+
+const ensureWindowPosition = mainWindowState => {
+  let { x, y, width, height } = mainWindowState
+  let center = false
+  if (x === undefined || y === undefined) {
+    center = true
+  } else {
+    center = !screen.getAllDisplays().map(display =>
+      x >= display.bounds.x && x <= display.bounds.x + display.bounds.width &&
+      y >= display.bounds.y && y <= display.bounds.y + display.bounds.height)
+      .some(display => display)
+  }
+  if (center) {
+    // win.center() and "workArea" doesn't work on Linux
+    const screenArea = process.platform === 'linux' ? screen.getPrimaryDisplay().bounds : screen.getPrimaryDisplay().workArea
+    x = Math.ceil(screenArea.x + (screenArea.width - width) / 2)
+    y = Math.ceil(screenArea.y + (screenArea.height - height) / 2)
+  }
+  return {
+    x,
+    y,
+    width,
+    height
+  }
 }
 
 export default createWindow
---
+++ src/renderer/assets/window-controls.js
@@ -0,0 +1,30 @@
+/*
+Copyright (c) GitHub, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// These paths are all drawn to a 10x10 view box and replicate the symbols
+// seen on Windows 10 window controls.
+export const closePath =
+  'M 0,0 0,0.7 4.3,5 0,9.3 0,10 0.7,10 5,5.7 9.3,10 10,10 10,9.3 5.7,5 10,0.7 10,0 9.3,0 5,4.3 0.7,0 Z'
+export const restorePath =
+  'm 2,1e-5 0,2 -2,0 0,8 8,0 0,-2 2,0 0,-8 z m 1,1 6,0 0,6 -1,0 0,-5 -5,0 z m -2,2 6,0 0,6 -6,0 z'
+export const maximizePath = 'M 0,0 0,10 10,10 10,0 Z M 1,1 9,1 9,9 1,9 Z'
+export const minimizePath = 'M 0,5 10,5 10,6 0,6 Z'
