42179e1c0fecfc5e05c7e13ec8b7bb43988be493
Jocs <luoran1988@126.com>
2018-04-04 11:32:43 +0800

    Optimization: user preference

--- src/main/preference.js
+++ src/main/preference.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { ipcMain, BrowserWindow } from 'electron'
 import { windows } from './createWindow'
-import { getPath, hasSameKeys, log, mkdir } from './utils'
+import { getPath, hasSameKeys, log, ensureDir } from './utils'
 
 const FILE_NAME = 'preference.md'
 const staticPath = path.join(__static, FILE_NAME)
@@ -13,87 +13,79 @@ class Preference {
     this.cache = null
     this.staticPath = staticPath
     this.userDataPath = userDataPath
-    this.requiresUpdate = false
 
-    if (!fs.existsSync(userDataPath)) {
-      mkdir(getPath('userData'))
+    this.init()
+  }
+
+  init () {
+    // If the preference.md is not existed or can not load json from it.
+    // Rebuild the preference.md
+    let userSetting = null
+    if (!fs.existsSync(userDataPath) || !this.loadJson(userDataPath)) {
+      ensureDir(getPath('userData'))
       const content = fs.readFileSync(staticPath, 'utf-8')
       fs.writeFileSync(userDataPath, content, 'utf-8')
     } else {
-      // load settings into cache
-      this.getAll()
-
-      // if needed update settings to latest version
-      this.update()
+      const defaultSettings = this.loadJson(staticPath)
+      userSetting = this.loadJson(userDataPath)
+      const requiresUpdate = !hasSameKeys(defaultSettings, userDataPath)
+      if (requiresUpdate) {
+        for (const key in userSetting) {
+          if (userSetting.hasOwnProperty(key) && !defaultSettings.hasOwnProperty(key)) {
+            delete userSetting[key]
+          }
+        }
+        Object.assign(userSetting, defaultSettings)
+        this.writeJson(userSetting, false)
+          .catch(log)
+      }
     }
+    if (!userSetting) userSetting = this.loadJson(userDataPath)
+    this.cache = userSetting
   }
 
   getAll () {
-    const { cache } = this
-    if (cache) {
-      return cache
-    } else {
-      const defaultSettings = this.loadJson(staticPath)
-      const userSettings = this.loadJson(userDataPath)
-      if (userSettings) {
-        // combine latest settings with user settings
-        this.cache = Object.assign({}, defaultSettings, userSettings)
-        // check if we have to update user settings
-        this.requiresUpdate = !hasSameKeys(defaultSettings, userSettings)
-        return this.cache
-      }
-      return defaultSettings
-    }
+    return this.cache
   }
 
   setItem (key, value) {
-    const { userDataPath } = this
     const preUserSetting = this.getAll()
     const newUserSetting = this.cache = Object.assign({}, preUserSetting, { [key]: value })
-
-    return new Promise((resolve, reject) => {
-      const content = fs.readFileSync(userDataPath, 'utf-8')
-      const tokens = content.split('```')
-      const newContent = tokens[0] +
-        '```json\n' +
-        JSON.stringify(newUserSetting, null, 2) +
-        '\n```' +
-        tokens[2]
-      fs.writeFile(userDataPath, newContent, 'utf-8', err => {
-        if (err) reject(err)
-        else resolve(newUserSetting)
-      })
-    })
-  }
-
-  update () {
-    if (this.requiresUpdate) {
-      this.requiresUpdate = false
-
-      // check if there are keys to remove
-      const defaultSettings = this.loadJson(staticPath)
-      Object.keys(this.cache).map(key => {
-        if (typeof defaultSettings[key] === 'undefined') {
-          delete this.cache[key]
-        }
-      })
-
-      // write latest settings to filesystem. 'setItem' will rewrite complete user settings.
-      const userSettings = this.getAll()
-      this.setItem('theme', userSettings.theme)
-    }
+    return this.writeJson(newUserSetting)
   }
 
   loadJson (filePath) {
     const JSON_REG = /```json(.+)```/g
-    const content = fs.readFileSync(filePath, 'utf-8')
     try {
+      const content = fs.readFileSync(filePath, 'utf-8')
       const userSetting = JSON_REG.exec(content.replace(/\n/g, ''))[1]
       return JSON.parse(userSetting)
     } catch (err) {
       log(err)
       return null
     }
+  }
+
+  writeJson (json, async = true) {
+    const { userDataPath } = this
+    return new Promise((resolve, reject) => {
+      const content = fs.readFileSync(userDataPath, 'utf-8')
+      const tokens = content.split('```')
+      const newContent = tokens[0] +
+        '```json\n' +
+        JSON.stringify(json, null, 2) +
+        '\n```' +
+        tokens[2]
+      if (async) {
+        fs.writeFile(userDataPath, newContent, 'utf-8', err => {
+          if (err) reject(err)
+          else resolve(json)
+        })
+      } else {
+        fs.writeFileSync(userDataPath, newContent, 'utf-8')
+        resolve(json)
+      }
+    })
   }
 }
 
--- src/main/utils.js
+++ src/main/utils.js
@@ -4,6 +4,17 @@ import fse from 'fs-extra'
 import { app, Menu } from 'electron'
 import { EXTENSIONS } from './config'
 
+// creates a directory if it doesn't already exist.
+export const ensureDir = dirPath => {
+  try {
+    fse.ensureDirSync(dirPath)
+  } catch (e) {
+    if (e.code !== 'EEXIST') {
+      throw e
+    }
+  }
+}
+
 export const getPath = directory => {
   return app.getPath(directory)
 }
@@ -17,7 +28,7 @@ export const log = data => {
   if (typeof data !== 'string') data = (data.stack || data).toString()
   const LOG_DATA_PATH = path.join(getPath('userData'), 'error.log')
   const LOG_TIME = new Date().toLocaleString()
-  mkdir(getPath('userData'))
+  ensureDir(getPath('userData'))
   fs.appendFileSync(LOG_DATA_PATH, `\n${LOG_TIME}\n${data}\n`)
 }
 
@@ -58,15 +69,4 @@ export const isFile = filepath => {
 // returns true if the file is a supported markdown file.
 export const isMarkdownFile = filepath => {
   return isFile(filepath) && hasMarkdownExtension(filepath)
-}
-
-// creates a directory if it doesn't already exist.
-export const mkdir = dirPath => {
-  try {
-    fse.ensureDirSync(dirPath)
-  } catch (e) {
-    if (e.code !== 'EEXIST') {
-      throw e
-    }
-  }
 }
