9e7b07b68d6f598296d9af9a799e14f0a1fbf29a
Felix Häusler <hfelix@protonmail.ch>
2018-04-08 12:52:29 +0200

    Disable tab focus and indent list items with tab (#119)

--- src/editor/contentState/tabCtrl.js
+++ src/editor/contentState/tabCtrl.js
@@ -22,7 +22,69 @@ const tabCtrl = ContentState => {
     return false
   }
 
+  ContentState.prototype.isIndentableListItem = function () {
+    const { start, end } = this.cursor
+    const startBlock = this.getBlock(start.key)
+    if (startBlock.type !== 'p' || !startBlock.parent) {
+      return false
+    }
+
+    const listItem = this.getParent(startBlock)
+    if (listItem.type !== 'li' || start.key !== end.key || start.offset !== end.offset) {
+      return false
+    }
+
+    // Now we know it's a list item. Check whether we can indent the list item.
+    const list = this.getParent(listItem)
+    return list && /ol|ul/.test(list.type) && list.children.indexOf(listItem) >= 1
+  }
+
+  ContentState.prototype.indentListItem = function () {
+    const { start } = this.cursor
+    const startBlock = this.getBlock(start.key)
+    const listItem = this.getParent(startBlock)
+    const list = this.getParent(listItem)
+    const prevListItem = this.getPreSibling(listItem)
+
+    this.removeBlock(listItem)
+
+    // Search for a list in previous block
+    let newList = this.getLastChild(prevListItem)
+    if (!newList || !/ol|ul/.test(newList.type)) {
+      newList = this.createBlock(list.type)
+      this.appendChild(prevListItem, newList)
+    }
+
+    // Update item type only if we insert into an existing list
+    if (newList.children.length !== 0) {
+      listItem.isLooseListItem = newList.children[0].isLooseListItem
+    }
+
+    this.appendChild(newList, listItem)
+    this.render()
+  }
+
+  // ContentState.prototype.insertTab = function () {
+  //   // TODO(fxha): Tabs and all spaces with length > 1 are converted to a single space
+  //                  in editor mode. Maybe write a HTML "tab" element
+  //   // TODO(fxha): create setting entry "tabSize: <int>"
+  //   const tabCharacter = new Array(4).join(' ')
+  //   const { start, end } = this.cursor
+  //   const startBlock = this.getBlock(start.key)
+  //   const endBlock = this.getBlock(end.key)
+  //   if (this.cursor.start.key === this.cursor.end.key) {
+  //     startBlock.text = startBlock.text.substring(0, start.offset) + tabCharacter + endBlock.text.substring(end.offset)
+  //     // workaround: see todo 1
+  //     this.cursor.start.offset += 1 // tabCharacter.length
+  //     this.cursor.end.offset += 1 // tabCharacter.length
+  //     this.render()
+  //   }
+  // }
+
   ContentState.prototype.tabHandler = function (event) {
+    // disable tab focus
+    event.preventDefault()
+
     const { start, end } = selection.getCursorRange()
     const startBlock = this.getBlock(start.key)
     const endBlock = this.getBlock(end.key)
@@ -33,7 +95,6 @@ const tabCtrl = ContentState => {
       nextCell = endBlock
     }
     if (nextCell) {
-      event.preventDefault()
       const key = nextCell.key
       const textLength = nextCell.text.length
       this.cursor = {
@@ -41,7 +102,11 @@ const tabCtrl = ContentState => {
         end: { key, offset: textLength }
       }
       this.render()
-    }
+    } else if (this.isIndentableListItem()) {
+      this.indentListItem()
+    } // else {
+    //   this.insertTab()
+    // }
   }
 }
 
