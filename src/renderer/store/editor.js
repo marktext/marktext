6596bd9016cd75993f2fed9bfa3e54c2b7b1ded2
Anderson <lhongda@live.com>
2018-04-17 23:44:54 +0800

    Update editor.js

--- src/renderer/store/editor.js
+++ src/renderer/store/editor.js
@@ -12,7 +12,7 @@ const state = {
   },
   // user preference
   theme: 'light',
-  editorFontFamily: 'Monaco',
+  editorFontFamily: 'Open Sans',
   fontSize: '16px',
   lineHeight: 1.6,
   lightColor: '#303133', // color in light theme
