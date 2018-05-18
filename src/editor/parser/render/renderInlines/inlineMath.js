a5826b49cf970d81c6b54955de66c8a42b6984e8
Felix Häusler <hfelix@protonmail.ch>
2018-05-18 16:07:17 +0200

    fix: #277 inline math

--- src/editor/parser/render/renderInlines/inlineMath.js
+++ src/editor/parser/render/renderInlines/inlineMath.js
@@ -1,3 +1,5 @@
+import { snackToCamel } from '../../../utils'
+
 export default function inlineMath (h, cursor, block, token, outerClass) {
-  return this['display_math'](h, cursor, block, token, outerClass)
+  return this[snackToCamel('display_math')](h, cursor, block, token, outerClass)
 }
