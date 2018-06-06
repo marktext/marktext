e4877adc42c100dc893269af72397a7cc946e3ef
Jocs <luoran1988@126.com>
2018-06-06 14:41:36 +0800

    feature: search in project

--- src/renderer/components/sideBar/search.vue
+++ src/renderer/components/sideBar/search.vue
@@ -1,13 +1,103 @@
 <template>
     <div class="side-bar-search">
-      Search Bar
+      <div class="search-wrapper">
+        <input
+          type="text" v-model="keyword"
+          placeholder="Search in project..."
+          @keyup="search"
+        >
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-search"></use>
+        </svg>
+      </div>
+      <div class="search-result" v-if="searchResult.length">
+        <list-file
+          v-for="(file, index) of searchResult"
+          :key="index"
+          :file="file"
+        ></list-file>
+      </div>
+      <div class="empty" v-else>
+        <span>{{ !keyword ? 'Input search keyword' : 'No matched files' }}</span>
+      </div>
     </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+  import ListFile from './listFile.vue'
 
+  export default {
+    data () {
+      return {
+        keyword: '',
+        searchResult: []
+      }
+    },
+    components: {
+      ListFile
+    },
+    computed: {
+      ...mapGetters(['fileList'])
+    },
+    methods: {
+      search () {
+        const { keyword } = this
+
+        if (!keyword) {
+          this.searchResult = []
+          return
+        }
+        this.searchResult = this.fileList.filter(f => f.data.markdown.indexOf(keyword) >= 0)
+      }
+    }
+  }
 </script>
 
 <style scoped>
-
+  .side-bar-search {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+  .search-wrapper {
+    display: flex;
+    margin: 35px 20px;
+    border-radius: 3px;
+    height: 30px;
+    border: 1px solid var(--lightBorder);
+    box-sizing: border-box;
+    align-items: center;
+    & > input {
+      height: 100%;
+      flex: 1;
+      border: none;
+      outline: none;
+      padding: 0 8px;
+      font-size: 13px;
+      width: 50%;
+    }
+    & > svg {
+      cursor: pointer;
+      flex-shrink: 0;
+      width: 20px;
+      height: 20px;
+      margin-right: 5px;
+      &:hover {
+        color: var(--brandColor);
+      }
+    }
+  }
+  .empty,
+  .search-result {
+    flex: 1;
+    overflow: auto;
+    &::-webkit-scrollbar:vertical {
+      width: 5px;
+    }
+  }
+  .empty {
+    font-size: 13px;
+    text-align: center;
+  }
 </style>
