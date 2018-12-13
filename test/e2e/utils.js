21243930e8ef9718f5bdd69d19e38129ffbd67a9
Felix Häusler <hfelix@protonmail.ch>
2018-12-13 14:56:10 +0100

    Fix build failure with latest vue version

--- test/e2e/utils.js
+++ test/e2e/utils.js
@@ -3,21 +3,20 @@ import { Application } from 'spectron'
 
 export default {
   afterEach () {
-    this.timeout(30000)
+    this.timeout(20000)
 
     if (this.app && this.app.isRunning()) {
       return this.app.stop()
     }
   },
   beforeEach () {
-    this.timeout(30000)
+    this.timeout(20000)
     this.app = new Application({
       path: electron,
       args: ['dist/electron/main.js'],
-      startTimeout: 30000,
-      waitTimeout: 30000
+      startTimeout: 20000,
+      waitTimeout: 20000
     })
-
     return this.app.start()
   }
 }
