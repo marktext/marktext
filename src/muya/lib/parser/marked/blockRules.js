04517cda91db2b9e82eb0bf20acbc1038e9f7342
Felix Häusler <hfelix@protonmail.ch>
2019-02-26 23:14:02 +0100

    markedjs#1023: new blockquote rule: match any paragraph-like content up to next line

--- src/muya/lib/parser/marked/blockRules.js
+++ src/muya/lib/parser/marked/blockRules.js
@@ -13,14 +13,14 @@ const block = {
   nptable: noop,
   // DOTO(@Jocs) need to support multiple line in setext heading?
   lheading: /^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/,
-  blockquote: /^( *>[^\n]+(\n(?!def)[^\n]+)*\n*)+/,
+  blockquote: /^( {0,3}> ?(paragraph|[^\n]*)(?:\n|$))+/,
   tasklist: /^( *)([*+-] \[(?:X|x|\s)\]) [\s\S]+?(?:hr|def|\n{2,}(?! )(?!\1(?:[*+-] \[(?:X|x|\s)\]))\n*|\s*$)/,
   orderlist: /^( *)(\d+\.) [\s\S]+?(?:hr|def|\n{2,}(?! )(?!\1\d+\. )\n*|\s*$)/,
   bulletlist: /^( *)([*+-]) [\s\S]+?(?:hr|def|\n{2,}(?! )(?!\1[*+-] )\n*|\s*$)/,
   html: /^ *(?:comment *(?:\n|\s*$)|closed *(?:\n{2,}|\s*$)|closing *(?:\n{2,}|\s*$))/,
   def: /^ {0,3}\[([^\]]+)\]: *<?([^\s>]+)>?(?: +["(]([^\n]+)[")])? *(?:\n+|$)/,
   table: noop,
-  paragraph: /^((?:[^\n]+\n?(?!hr|heading|lheading|blockquote|tag|def))+)\n*/,
+  paragraph: /^([^\n]+(?:\n?(?!hr|heading|lheading| {0,3}>|tag)[^\n]+)+)/,
   text: /^[^\n]+/,
   frontmatter: /^---\n([\s\S]+?)---(?:\n+|$)/,
   multiplemath: /^\$\$\n([\s\S]+?)\n\$\$(?:\n+|$)/
@@ -35,8 +35,6 @@ block.item = replace(block.item, 'gm')(/bull/g, block.bullet)()
   block[list] = replace(block[list])('hr', '\\n+(?=\\1?(?:[-*_] *){3,}(?:\\n+|$))')('def', '\\n+(?=' + block.def.source + ')')()
 })
 
-block.blockquote = replace(block.blockquote)('def', block.def)()
-
 block._tag = '(?!(?:' +
   'a|em|strong|small|s|cite|q|dfn|abbr|data|time|code' +
   '|var|samp|kbd|sub|sup|i|b|u|mark|ruby|rt|rp|bdi|bdo' +
@@ -44,7 +42,9 @@ block._tag = '(?!(?:' +
 
 block.html = replace(block.html)('comment', /<!--[\s\S]*?-->/)('closed', /<(tag)[\s\S]+?<\/\1>/)('closing', /<tag(?:"[^"]*"|'[^']*'|[^'">])*?>/)(/tag/g, block._tag)()
 
-block.paragraph = replace(block.paragraph)('hr', block.hr)('heading', block.heading)('lheading', block.lheading)('blockquote', block.blockquote)('tag', '<' + block._tag)('def', block.def)()
+block.paragraph = replace(block.paragraph)('hr', block.hr)('heading', block.heading)('lheading', block.lheading)('tag', '<' + block._tag)('def', block.def)()
+
+block.blockquote = replace(block.blockquote)('paragraph', block.paragraph)()
 
 /**
  * Normal Block Grammar
