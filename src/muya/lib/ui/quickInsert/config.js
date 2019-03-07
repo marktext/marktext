7abb13778ea8dda8a06fdbf70049fd5e0d16cd5b
jocs <ransixi@gmail.com>
2019-03-08 00:31:44 +0800

    add windows support

--- src/muya/lib/ui/quickInsert/config.js
+++ src/muya/lib/ui/quickInsert/config.js
@@ -19,6 +19,9 @@ import orderListIcon from '../../assets/icons/order_list.svg'
 // import sequenceIcon from '../../assets/icons/sequence.svg'
 // import mermaidIcon from '../../assets/icons/mermaid.svg'
 // import vegaIcon from '../../assets/icons/chart.svg'
+import { isOsx } from '../../config'
+
+const COMMAND_KEY = isOsx ? '⌘' : '⌃'
 
 // Command (or Cmd) ⌘
 // Shift ⇧
@@ -58,21 +61,21 @@ export const quicInsertObj = {
     title: 'Paragraph',
     subTitle: 'Lorem Ipsum is simply dummy text',
     label: 'paragraph',
-    shortCut: '⌘0',
+    shortCut: `${COMMAND_KEY}0`,
     icon: paragraphIcon,
     color: 'rgb(224, 54, 54)'
   }, {
     title: 'Horizontal Line',
     subTitle: '---',
     label: 'hr',
-    shortCut: '⌥⌘-',
+    shortCut: `⌥${COMMAND_KEY}-`,
     icon: hrIcon,
     color: 'rgb(255, 83, 77)'
   }, {
     title: 'Front Matter',
     subTitle: '--- Lorem Ipsum ---',
     label: 'front-matter',
-    shortCut: '⌥⌘Y',
+    shortCut: `⌥${COMMAND_KEY}Y`,
     icon: frontMatterIcon,
     color: 'rgb(37, 198, 252)'
   }],
@@ -80,42 +83,42 @@ export const quicInsertObj = {
     title: 'Header 1',
     subTitle: '# Lorem Ipsum is simply ...',
     label: 'heading 1',
-    shortCut: '⌘1',
+    shortCut: `${COMMAND_KEY}1`,
     icon: header1Icon,
     color: 'rgb(86, 163, 108)'
   }, {
     title: 'Header 2',
     subTitle: '## Lorem Ipsum is simply ...',
     label: 'heading 2',
-    shortCut: '⌘2',
+    shortCut: `${COMMAND_KEY}2`,
     icon: header2Icon,
     color: 'rgb(94, 133, 121)'
   }, {
     title: 'Header 3',
     subTitle: '### Lorem Ipsum is simply ...',
     label: 'heading 3',
-    shortCut: '⌘3',
+    shortCut: `${COMMAND_KEY}3`,
     icon: header3Icon,
     color: 'rgb(119, 195, 79)'
   }, {
     title: 'Header 4',
     subTitle: '#### Lorem Ipsum is simply ...',
     label: 'heading 4',
-    shortCut: '⌘4',
+    shortCut: `${COMMAND_KEY}4`,
     icon: header4Icon,
     color: 'rgb(46, 104, 170)'
   }, {
     title: 'Header 5',
     subTitle: '##### Lorem Ipsum is simply ...',
     label: 'heading 5',
-    shortCut: '⌘5',
+    shortCut: `${COMMAND_KEY}5`,
     icon: header5Icon,
     color: 'rgb(126, 136, 79)'
   }, {
     title: 'Header 6',
     subTitle: '###### Lorem Ipsum is simply ...',
     label: 'heading 6',
-    shortCut: '⌘6',
+    shortCut: `${COMMAND_KEY}6`,
     icon: header6Icon,
     color: 'rgb(29, 176, 184)'
   }],
@@ -123,35 +126,35 @@ export const quicInsertObj = {
     title: 'Table Block',
     subTitle: '|Lorem | Ipsum is simply |',
     label: 'table',
-    shortCut: '⌘T',
+    shortCut: `${COMMAND_KEY}T`,
     icon: tableIcon,
     color: 'rgb(13, 23, 64)'
   }, {
     title: 'Mathematical Formula',
     subTitle: '$$ Lorem Ipsum is simply $$',
     label: 'mathblock',
-    shortCut: '⌥⌘M',
+    shortCut: `⌥${COMMAND_KEY}M`,
     icon: mathblockIcon,
     color: 'rgb(252, 214, 146)'
   }, {
     title: 'HTML Block',
     subTitle: '<div> Lorem Ipsum is simply </div>',
     label: 'html',
-    shortCut: '⌥⌘J',
+    shortCut: `⌥${COMMAND_KEY}J`,
     icon: htmlIcon,
     color: 'rgb(13, 23, 64)'
   }, {
     title: 'Code Block',
     subTitle: '```java Lorem Ipsum is simply ```',
     label: 'pre',
-    shortCut: '⌥⌘C',
+    shortCut: `⌥${COMMAND_KEY}C`,
     icon: codeIcon,
     color: 'rgb(164, 159, 147)'
   }, {
     title: 'Quote Block',
     subTitle: '>Lorem Ipsum is simply ...',
     label: 'blockquote',
-    shortCut: '⌥⌘Q',
+    shortCut: `⌥${COMMAND_KEY}Q`,
     icon: quoteIcon,
     color: 'rgb(31, 111, 181)'
   }],
@@ -159,21 +162,21 @@ export const quicInsertObj = {
     title: 'Order List',
     subTitle: '1. Lorem Ipsum is simply ...',
     label: 'ol-order',
-    shortCut: '⌥⌘O',
+    shortCut: `⌥${COMMAND_KEY}O`,
     icon: orderListIcon,
     color: 'rgb(242, 159, 63)'
   }, {
     title: 'Bullet List',
     subTitle: '- Lorem Ipsum is simply ...',
     label: 'ul-bullet',
-    shortCut: '⌥⌘U',
+    shortCut: `⌥${COMMAND_KEY}U`,
     icon: bulletListIcon,
     color: 'rgb(242, 117, 63)'
   }, {
     title: 'To-do List',
     subTitle: '- [x] Lorem Ipsum is simply ...',
     label: 'ul-task',
-    shortCut: '⌥⌘X',
+    shortCut: `⌥${COMMAND_KEY}X`,
     icon: todoListIcon,
     color: 'rgb(222, 140, 124)'
   }]
