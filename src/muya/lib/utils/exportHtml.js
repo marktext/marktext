ed71ab977982be57403fdf707da14d8864cf0572
Felix Häusler <hfelix@protonmail.ch>
2019-03-07 17:07:28 +0100

    Wrap code block lines when printing

--- src/muya/lib/utils/exportHtml.js
+++ src/muya/lib/utils/exportHtml.js
@@ -88,10 +88,6 @@ class ExportHtml {
       margin-top: 0;
       display: inline-block;
     }
-    .markdown-body pre code.fenced-code-block {
-      white-space: pre-wrap;
-      word-break: break-word;
-    }
     @media (max-width: 767px) {
       .markdown-body {
         padding: 15px;
