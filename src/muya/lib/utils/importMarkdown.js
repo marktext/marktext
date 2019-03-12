b3170c8dc954c53d313866c6fb2596cf5d4401ce
jocs <ransixi@gmail.com>
2019-03-13 00:12:13 +0800

    fix typo

--- src/muya/lib/utils/importMarkdown.js
+++ src/muya/lib/utils/importMarkdown.js
@@ -233,7 +233,7 @@ const importRegister = ContentState => {
     const turndownService = new TurndownService(turndownConfig)
     usePluginAddRules(turndownService)
     // remove double `\\` in Math but I dont know why there are two '\' when paste. @jocs
-    // fix #752, but I don't know why the &nbsp; valished.
+    // fix #752, but I don't know why the &nbsp; vanlished.
     html = html.replace(/&nbsp;/g, ' ')
     const markdown = turndownService.turndown(html) // .replace(/(\\)\\/g, '$1')
     return markdown
