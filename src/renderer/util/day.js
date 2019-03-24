98c4da1e246fcde08f1b388a57799b1b9cfb94ff
jocs <ransixi@gmail.com>
2019-03-25 01:25:18 +0800

    update: electron and dayjs

--- src/renderer/util/day.js
+++ src/renderer/util/day.js
@@ -2,7 +2,7 @@ import dayjs from 'dayjs/esm'
 import relativeTime from 'dayjs/esm/plugin/relativeTime'
 import 'dayjs/esm/locale/en' // load on demand
 
-dayjs.locale('en') // use Spanish locale globally
+dayjs.locale('en') // use en locale globally
 
 dayjs.extend(relativeTime)
 
