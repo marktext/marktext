2389332e427cf959a35d571341ea2f140f0bc59e
Ran Luo <luoran1988@126.com>
2019-03-25 12:03:45 +0800

    update: electron and dayjs (#801)
    * update: electron and dayjs
    
    * update useage of dayjs
    
    * rm yarn-error.log

--- src/renderer/util/day.js
+++ src/renderer/util/day.js
@@ -1,8 +1,9 @@
 import dayjs from 'dayjs/esm'
 import relativeTime from 'dayjs/esm/plugin/relativeTime'
-import 'dayjs/esm/locale/en' // load on demand
 
-dayjs.locale('en') // use Spanish locale globally
+// `en` is a built in locale. no need to import it.
+// import 'dayjs/esm/locale/en' // load on demand
+// dayjs.locale('en') // use en locale globally
 
 dayjs.extend(relativeTime)
 
