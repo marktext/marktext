30adfcbb57ee643cc4f9759e294604d7bb024c01
jocs <ransixi@gmail.com>
2019-04-04 01:16:36 +0800

    add comment

--- src/muya/lib/config/index.js
+++ src/muya/lib/config/index.js
@@ -242,6 +242,7 @@ export const isOsx = window && window.navigator && /Mac/.test(window.navigator.p
 // http[s] (domain or IPv4 or localhost or IPv6) [port] /not-white-space
 export const URL_REG = /^http(s)?:\/\/([a-z0-9\-._~]+\.[a-z]{2,}|[0-9.]+|localhost|\[[a-f0-9.:]+\])(:[0-9]{1,5})?\/[\S]+/i
 
+// selected from https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes
 export const WHITELIST_ATTRIBUTES = [
   'align', 'alt', 'checked', 'class', 'color', 'dir', 'disabled', 'for', 'height', 'hidden',
   'href', 'id', 'lang', 'lazyload', 'rel', 'spellcheck', 'src', 'srcset', 'start', 'style',
