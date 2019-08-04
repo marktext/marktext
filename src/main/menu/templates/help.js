e2bac53e662944f223e9232b20cb65f1d8087918
Felix Häusler <hfelix@protonmail.ch>
2019-08-04 16:51:50 +0200

    Improve updatable packages detection (#1230)

--- src/main/menu/templates/help.js
+++ src/main/menu/templates/help.js
@@ -4,6 +4,29 @@ import { isFile } from 'common/filesystem'
 import * as actions from '../actions/help'
 import { checkUpdates } from '../actions/marktext'
 
+/// Check whether the package is updatable at runtime.
+const isUpdatable = () => {
+  // TODO: If not updatable, allow to check whether there is a new version available.
+
+  const resFile = isFile(path.join(process.resourcesPath, 'app-update.yml'))
+  if (!resFile) {
+    // No update resource file available.
+    return false
+  } else if (process.env.APPIMAGE) {
+    // We are running as AppImage.
+    return true
+  } else if (process.platform === 'win32' && isFile(path.join(process.resourcesPath, 'md.ico'))) {
+    // Windows is a little but tricky. The update resource file is always available and
+    // there is no way to check the target type at runtime (electron-builder#4119).
+    // As workaround we check whether "md.ico" exists that is only included in the setup.
+    return true
+  }
+
+  // Otherwise assume that we cannot perform an auto update (standalone binary, archives,
+  // packed for package manager).
+  return false
+}
+
 export default function () {
   const helpMenu = {
     label: 'Help',
@@ -55,8 +78,7 @@ export default function () {
     }]
   }
 
-  if (isFile(path.join(process.resourcesPath, 'app-update.yml')) &&
-    (process.platform === 'win32' || !!process.env.APPIMAGE)) {
+  if (isUpdatable()) {
     helpMenu.submenu.push({
       type: 'separator'
     }, {
