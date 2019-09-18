6397d65f0ac552432c7a09bd036b66fd0963a10e
罗冉 <luoran@shimo.im>
2019-09-18 15:22:11 +0800

    remove debug codes

--- src/muya/lib/utils/importMarkdown.js
+++ src/muya/lib/utils/importMarkdown.js
@@ -77,7 +77,6 @@ const importRegister = ContentState => {
       children: []
     }
     const tokens = new Lexer({ disableInline: true }).lex(markdown)
-    console.log(JSON.stringify(tokens, null, 2))
     let token
     let block
     let value
