43ca0ce6f3d580285e0cf020e6d88ce4ebbb4ac4
Felix Häusler <hfelix@protonmail.ch>
2019-09-20 21:19:32 +0200

    Add system emoji fallback fonts

--- src/renderer/config.js
+++ src/renderer/config.js
@@ -1,11 +1,10 @@
 import path from 'path'
-import { isLinux } from './util'
 export const PATH_SEPARATOR = path.sep
 
 export const THEME_STYLE_ID = 'ag-theme'
 export const COMMON_STYLE_ID = 'ag-common-style'
 
-export const DEFAULT_EDITOR_FONT_FAMILY = `"Open Sans", "Clear Sans", "Helvetica Neue", Helvetica, Arial, sans-serif ${isLinux ? ', "Noto Color Emoji"' : ''}`
+export const DEFAULT_EDITOR_FONT_FAMILY = '"Open Sans", "Clear Sans", "Helvetica Neue", Helvetica, Arial, sans-serif, Segoe UI Emoji, Apple Color Emoji, "Noto Color Emoji"'
 export const DEFAULT_CODE_FONT_FAMILY = '"DejaVu Sans Mono", "Source Code Pro", "Droid Sans Mono", monospace'
 export const DEFAULT_STYLE = {
   codeFontFamily: DEFAULT_CODE_FONT_FAMILY,
