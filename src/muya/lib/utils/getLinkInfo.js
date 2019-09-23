6ea8cca3a479762ee001a113baccff97deee267a
罗冉 <luoran@shimo.im>
2019-09-23 15:26:48 +0800

    Feat: add link tooks: unlink and jump

---
+++ src/muya/lib/utils/getLinkInfo.js
@@ -0,0 +1,19 @@
+import { findNearestParagraph } from '../selection/dom'
+import { tokenizer } from '../parser'
+
+export const getLinkInfo = a => {
+  const paragraph = findNearestParagraph(a)
+  const raw = a.getAttribute('data-raw')
+  const start = a.getAttribute('data-start')
+  const end = a.getAttribute('data-end')
+  const tokens = tokenizer(raw)
+  const token = tokens[0]
+  token.range = {
+    start,
+    end
+  }
+  return {
+    key: paragraph.id,
+    token
+  }
+}
