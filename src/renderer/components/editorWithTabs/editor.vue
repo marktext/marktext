05a2a7f69caba487348a7ef081f13901962b5e6e
罗冉 <luoran@shimo.im>
2019-09-25 11:58:38 +0800

    Opti: support 1 * 1 table

--- src/renderer/components/editorWithTabs/editor.vue
+++ src/renderer/components/editorWithTabs/editor.vue
@@ -43,7 +43,7 @@
             size="mini"
             v-model="tableChecker.rows"
             controls-position="right"
-            :min="2"
+            :min="1"
             :max="20"
           ></el-input-number>
         </el-form-item>
@@ -52,7 +52,7 @@
             size="mini"
             v-model="tableChecker.columns"
             controls-position="right"
-            :min="2"
+            :min="1"
             :max="20"
           ></el-input-number>
         </el-form-item>
