ff1da05e48762ade9a5f250331d8dafea727f429
Ran Luo <ransixi@gmail.com>
2019-10-09 02:10:58 +0800

    Fix not fail e2e XSS test (#1458)

--- test/e2e/specs/xss.spec.js
+++ test/e2e/specs/xss.spec.js
@@ -4,8 +4,23 @@ describe('Cross-site Scripting Test', function () {
   beforeEach(utils.beforeXss)
   afterEach(utils.afterEach)
 
-  it('Load malicious document', function (done) {
-    setTimeout(() => {
+  it('Load malicious document', function () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, 3000)
+    })
+      .then(() => {
+        return this.app.client.getRenderProcessLogs()
+        .then(function (logs) {
+          const xssErrorCount = logs.filter(log => {
+            return log.level === 'SEVERE' && /XSS/i.test(log.message) && log.source === 'javascript'
+          }).length
+          expect(xssErrorCount).to.equal(0)
+        })
+      })
+    // setTimeout(() => {
+      
       // If a test fails, an exception is thrown.
 
       // TODO: The process is not terminated if a test fails because the connection to Electron is lost (spectron bug?).
@@ -15,7 +30,6 @@ describe('Cross-site Scripting Test', function () {
       //     Error: Request timed out after the element was still found on the page.
       //         at execute(<Function>, "require") - api.js:63:26
 
-      done()
-    }, 3000)
+    // }, 3000)
   })
 })
