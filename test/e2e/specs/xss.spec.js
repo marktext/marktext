db0f7a72383f284e3e8296ec72dad0238cf768b6
Felix Häusler <hfelix@protonmail.ch>
2019-10-10 21:20:49 +0200

    Try to fix E2E test on Linux worker

--- test/e2e/specs/xss.spec.js
+++ test/e2e/specs/xss.spec.js
@@ -4,20 +4,18 @@ describe('Cross-site Scripting Test', function () {
   beforeEach(utils.beforeXss)
   afterEach(utils.afterEach)
 
-  it('Load malicious document', function () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve()
-      }, 3000)
-    })
-      .then(() => {
-        return this.app.client.getRenderProcessLogs()
-          .then(function (logs) {
-            const xssErrorCount = logs.filter(log => {
-              return log.level === 'SEVERE' && /XSS/i.test(log.message) && log.source === 'javascript'
-            }).length
-            expect(xssErrorCount).to.equal(0)
-          })
-      })
+  it('Load malicious document', function (done) {
+    setTimeout(() => {
+      this.app.client.getRenderProcessLogs()
+        .then(function (logs) {
+          const xssErrorCount = logs.filter(log => {
+            return log.level === 'SEVERE' && /XSS/i.test(log.message) && log.source === 'javascript'
+          }).length
+
+          // This will result in a timeout when a test fails.
+          expect(xssErrorCount).to.equal(0)
+          done()
+        })
+    }, 3000)
   })
 })
--- test/e2e/utils.js
+++ test/e2e/utils.js
@@ -3,29 +3,29 @@ import { Application } from 'spectron'
 
 export default {
   afterEach () {
-    this.timeout(20000)
+    this.timeout(25000)
 
     if (this.app && this.app.isRunning()) {
       return this.app.stop()
     }
   },
   beforeEach () {
-    this.timeout(20000)
+    this.timeout(25000)
     this.app = new Application({
       path: electron,
       args: ['dist/electron/main.js'],
-      startTimeout: 20000,
-      waitTimeout: 20000
+      startTimeout: 25000,
+      waitTimeout: 25000
     })
     return this.app.start()
   },
   beforeXss () {
-    this.timeout(20000)
+    this.timeout(25000)
     this.app = new Application({
       path: electron,
       args: ['dist/electron/main.js', 'test/e2e/data/xss.md'],
-      startTimeout: 20000,
-      waitTimeout: 20000
+      startTimeout: 25000,
+      waitTimeout: 25000
     })
     return this.app.start()
   }
