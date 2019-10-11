9f074c3bfb989b6cd386cdabb1d2fbec37909c5a
jocs <ransixi@gmail.com>
2019-10-11 20:11:47 +0800

    Change table delete icon

--- src/muya/lib/parser/render/renderBlock/renderToolBar.js
+++ src/muya/lib/parser/render/renderBlock/renderToolBar.js
@@ -5,7 +5,7 @@ import TableIcon from '../../../assets/pngicon/table/table@2x.png'
 import AlignLeftIcon from '../../../assets/pngicon/algin_left/2.png'
 import AlignRightIcon from '../../../assets/pngicon/algin_right/2.png'
 import AlignCenterIcon from '../../../assets/pngicon/algin_center/2.png'
-import DeleteIcon from '../../../assets/pngicon/delete/2.png'
+import DeleteIcon from '../../../assets/pngicon/table_delete/2.png'
 
 export const TABLE_TOOLS = [{
   label: 'table',
