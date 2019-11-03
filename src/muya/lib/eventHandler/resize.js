c3946b4da953f0570baa7f38ccdcda3f0861ee23
jocs <ransixi@gmail.com>
2019-11-03 13:50:40 +0800

    use throttle util

--- src/muya/lib/eventHandler/resize.js
+++ src/muya/lib/eventHandler/resize.js
@@ -1,27 +1,21 @@
 import resizeCodeBlockLineNumber from '../utils/resizeCodeLineNumber'
+import { throttle } from '../utils'
 
 class Resize {
   constructor (muya) {
     this.muya = muya
-    this.timer = null
     this.listen()
   }
 
   listen () {
-    window.addEventListener('resize', function () {
-      if (this.timer) {
-        clearTimeout(this.timer)
-      }
-      this.timer = setTimeout(() => {
-        console.log('wwww')
-        const codeBlocks = document.querySelectorAll('pre.line-numbers')
-        if (codeBlocks.length) {
-          for (const ele of codeBlocks) {
-            resizeCodeBlockLineNumber(ele)
-          }
+    window.addEventListener('resize', throttle(() => {
+      const codeBlocks = document.querySelectorAll('pre.line-numbers')
+      if (codeBlocks.length) {
+        for (const ele of codeBlocks) {
+          resizeCodeBlockLineNumber(ele)
         }
-      }, 300)
-    })
+      }
+    }, 300))
   }
 }
 
