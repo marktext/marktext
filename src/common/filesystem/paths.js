446620dff462e7eb43e13f07713e4f19a991400e
Ziqiang Li <ziqiang.lee@gmail.com>
2019-11-15 00:00:53 +0800

    Fixe: https://github.com/marktext/marktext/issues/1696 (#1697)

--- src/common/filesystem/paths.js
+++ src/common/filesystem/paths.js
@@ -31,7 +31,7 @@ export const IMAGE_EXTENSIONS = [
  */
 export const hasMarkdownExtension = filename => {
   if (!filename || typeof filename !== 'string') return false
-  return MARKDOWN_EXTENSIONS.some(ext => filename.endsWith(`.${ext}`))
+  return MARKDOWN_EXTENSIONS.some(ext => filename.toLowerCase().endsWith(`.${ext}`))
 }
 
 /**
