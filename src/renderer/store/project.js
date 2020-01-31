36bd912466bdc696f46123e609dfe43cc5a67a82
Davis Riedel <mail@davis-riedel.de>
2020-01-31 15:37:41 +0100

    Use hasMarkdownExtension function everywhere an extension has to be checked.
    Allowed markdown extensions are handeled in several places in different ways. Use the hasMarkdownFunction from common/filesystem/paths.js to allow to easily update the allowed extensions when needed.

--- src/renderer/store/project.js
+++ src/renderer/store/project.js
@@ -6,6 +6,7 @@ import { create, paste, rename } from '../util/fileSystem'
 import { PATH_SEPARATOR } from '../config'
 import notice from '../services/notification'
 import { getFileStateFromData } from './help'
+import { hasMarkdownExtension } from '../../common/filesystem/paths'
 
 const state = {
   activeItem: {},
@@ -174,9 +175,8 @@ const actions = {
   CREATE_FILE_DIRECTORY ({ commit, state }, name) {
     const { dirname, type } = state.createCache
 
-    if (type === 'file') {
-      const CHECK_MD_EXTENSION = /.*\.(?:markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text|txt)$/i
-      if (!CHECK_MD_EXTENSION.test(name)) name += '.md'
+    if (type === 'file' && !hasMarkdownExtension(name)) {
+      name += '.md'
     }
 
     const fullName = `${dirname}/${name}`
