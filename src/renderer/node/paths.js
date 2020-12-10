1a66f8521ccd252be0ec89173cb6768f7d9c0d86
Felix Häusler <hfelix@protonmail.ch>
2020-12-11 00:35:28 +0100

    Update Electron and remove workaround

--- src/renderer/node/paths.js
+++ src/renderer/node/paths.js
@@ -1,19 +1,8 @@
-import path from 'path'
-// import { rgPath } from 'vscode-ripgrep'
+import { rgPath } from 'vscode-ripgrep'
 import EnvPaths from 'common/envPaths'
-import { getResourcesPath } from 'common/filesystem/paths'
 
-// WORKAROUND: We are unable to load vscode-ripgrep when it's bundled into the asar since Electron 11 (electron-builder#5360).
-let rgDiskPath
-if (process.env.NODE_ENV === 'development') {
-  rgDiskPath = require('vscode-ripgrep').rgPath
-} else {
-  rgDiskPath = path.join(getResourcesPath(), `app.asar.unpacked/node_modules/vscode-ripgrep/bin/rg${process.platform === 'win32' ? '.exe' : ''}`)
-}
-// END WORKAROUND
-
-// // // "vscode-ripgrep" is unpacked out of asar because of the binary.
-// const rgDiskPath = rgPath.replace(/\bapp\.asar\b/, 'app.asar.unpacked')
+// // "vscode-ripgrep" is unpacked out of asar because of the binary.
+const rgDiskPath = rgPath.replace(/\bapp\.asar\b/, 'app.asar.unpacked')
 
 class RendererPaths extends EnvPaths {
   /**
