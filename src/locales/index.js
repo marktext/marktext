0b7573657170abd0b6a8fd8a95b99d7f0eab58f6
AnClark <clarklaw4701@qq.com>
2021-12-11 12:46:34 +0800

    i18n: Share the same i18n object & add i18n for Muya
    Both main and renderer process now use the same vue-i18n object
    definition. They will be instanciated in different instances in each
    process or window.
    
    Also added i18n module for Muya.

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,11 +1,26 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+
 const messages = {
   zh: require('./zh.json'),
   en: require('./en.json')
 }
 
 const DEFAULT_LOCALE = 'en'
+let currentLocale = 'zh' // TODO: Get it from Vue storage
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: currentLocale || DEFAULT_LOCALE, // set locale
+  fallbackLocale: 'en',
+  messages
+})
 
 export {
   messages,
   DEFAULT_LOCALE
 }
+
+export default i18n
--- src/main/i18n/index.js
+++ src/main/i18n/index.js
@@ -1,16 +1,3 @@
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
-import { messages, DEFAULT_LOCALE } from '../../locales'
-
-Vue.use(VueI18n)
-
-let currentLocale = 'zh' // TODO: Get it from Vue storage
-
-// Create VueI18n instance with options
-const i18n = new VueI18n({
-  locale: currentLocale || DEFAULT_LOCALE, // set locale
-  fallbackLocale: 'en',
-  messages
-})
+import i18n from '../../locales'
 
 export default i18n
