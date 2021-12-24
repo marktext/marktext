68b1f0bc14a94e8e070497e3beaaa1da7f0f85ff
Felix Häusler <hfelix@protonmail.ch>
2021-12-24 13:40:45 +0100

    Enable macOS arm64 build target

--- .electron-vue/dev-runner.js
+++ .electron-vue/dev-runner.js
@@ -159,8 +159,8 @@ function greeting () {
   const cols = process.stdout.columns
   let text = ''
 
-  if (cols > 104) text = 'electron-vue'
-  else if (cols > 76) text = 'electron-|vue'
+  if (cols > 104) text = 'building marktext'
+  else if (cols > 76) text = 'building|marktext'
   else text = false
 
   if (text) {
@@ -169,7 +169,9 @@ function greeting () {
       font: 'simple3d',
       space: false
     })
-  } else console.log(chalk.yellow.bold('\n  electron-vue'))
+  } else {
+    console.log(chalk.yellow.bold('\n  building marktext'))
+  }
   console.log(chalk.blue('  getting ready...') + '\n')
 }
 
