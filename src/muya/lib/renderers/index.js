fa5cd82455d12860bd31e99240c83d25eb2a9b32
jocs <ransixi@gmail.com>
2022-01-05 00:44:14 +0800

    fix: fix export mermaid pie chart error

--- src/muya/lib/renderers/index.js
+++ src/muya/lib/renderers/index.js
@@ -20,7 +20,7 @@ const loadRenderer = async (name) => {
         rendererCache.set(name, m.default)
         break
       case 'mermaid':
-        m = await import('mermaid/dist/mermaid.core')
+        m = await import('mermaid/dist/mermaid.core.js')
         rendererCache.set(name, m.default)
         break
       case 'vega-lite':
