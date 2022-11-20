0c7701934f79562ceba44eeff09f6e447f5c4340
xigaoku <110461994+xigaoku@users.noreply.github.com>
2022-11-20 22:47:15 +0800

    update mermaid version from 8.14.0 to 9.2.2

--- src/muya/lib/renderers/index.js
+++ src/muya/lib/renderers/index.js
@@ -20,7 +20,7 @@ const loadRenderer = async (name) => {
         rendererCache.set(name, m.default)
         break
       case 'mermaid':
-        m = await import('mermaid/dist/mermaid.core.js')
+        m = await import('mermaid/dist/mermaid.min.js')
         rendererCache.set(name, m.default)
         break
       case 'vega-lite':
