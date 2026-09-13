8c79b4c78581a2e5f1041053d9a30c4dc2f1f68a
dronov-dmitry <dronov-dmitry@users.noreply.github.com>
2026-09-13 15:23:06 +0200

    fix: isolate LTR content in title bar to prevent bidi reordering on Windows
    Wrap the title bar filename and breadcrumb spans inside a `<bdi dir=ltr>`
    element to isolate LTR content from the outer RTL container. This prevents
    the Unicode Bidi Algorithm from reordering characters in filenames
    (e.g. "003_report.md" was displayed as "report.md_003" on Windows
    where breadcrumb segments don't render).
    
    - Keep `direction: rtl` on the outer span for GH#339 left-side clipping
    - Add `unicode-bidi: isolate` on inner spans to prevent RTL inheritance
    - Move save-dot margin from right to left (correct positioning in LTR)
    
    Co-Authored-By: dronov-dmitry <dronov-dmitry@users.noreply.github.com>

--- packages/desktop/src/renderer/src/components/titleBar/index.vue
+++ packages/desktop/src/renderer/src/components/titleBar/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <div
-      v-if="showTitleBar"
-      class="title-bar-editor-bg"
-      :class="{ 'tabs-visible': showTabBar }"
-    />
+    <div v-if="showTitleBar" class="title-bar-editor-bg" :class="{ 'tabs-visible': showTabBar }" />
     <div
       v-if="showTitleBar"
       class="title-bar"
@@ -15,35 +11,21 @@
         { isOsx: isOsx }
       ]"
     >
-      <div
-        class="title"
-        @dblclick.stop="toggleMaxmizeOnMacOS"
-      >
+      <div class="title" @dblclick.stop="toggleMaxmizeOnMacOS">
         <span v-if="!filename">MarkText</span>
         <span v-else>
-          <span
-            v-for="(path, index) of paths"
-            :key="index"
-          >
-            {{ path }}
-            <el-icon
-              class="path-arrow"
-              :size="12"
-            >
-              <ArrowRight />
-            </el-icon>
-          </span>
-          <span
-            class="filename"
-            :class="{ isOsx: platform === 'darwin' }"
-            @click="rename"
-          >
-            {{ filename }}
-          </span>
-          <span
-            class="save-dot"
-            :class="{ show: !isSaved }"
-          />
+          <bdi dir="ltr">
+            <span v-for="(path, index) of paths" :key="index">
+              {{ path }}
+              <el-icon class="path-arrow" :size="12">
+                <ArrowRight />
+              </el-icon>
+            </span>
+            <span class="filename" :class="{ isOsx: platform === 'darwin' }" @click="rename">
+              {{ filename }}
+            </span>
+            <span class="save-dot" :class="{ show: !isSaved }" />
+          </bdi>
         </span>
       </div>
       <div :class="showCustomTitleBar ? 'left-toolbar title-no-drag' : 'right-toolbar'">
@@ -62,20 +44,19 @@
         >
           <template #content>
             <div class="title-item">
-              <span class="front">{{ t('menu.counter.words') }}:</span><span class="text">{{ wordCount['word'] }}</span>
+              <span class="front">{{ t('menu.counter.words') }}:</span
+              ><span class="text">{{ wordCount['word'] }}</span>
             </div>
             <div class="title-item">
-              <span class="front">{{ t('menu.counter.characters') }}:</span><span class="text">{{ wordCount['character'] }}</span>
+              <span class="front">{{ t('menu.counter.characters') }}:</span
+              ><span class="text">{{ wordCount['character'] }}</span>
             </div>
             <div class="title-item">
-              <span class="front">{{ t('menu.counter.paragraphs') }}:</span><span class="text">{{ wordCount['paragraph'] }}</span>
+              <span class="front">{{ t('menu.counter.paragraphs') }}:</span
+              ><span class="text">{{ wordCount['paragraph'] }}</span>
             </div>
           </template>
-          <div
-            v-if="wordCount"
-            class="word-count"
-            @click.stop="handleWordClick"
-          >
+          <div v-if="wordCount" class="word-count" @click.stop="handleWordClick">
             <span class="text-center-vertical">{{ `${HASH[show].short} ${wordCount[show]}` }}</span>
           </div>
         </el-tooltip>
@@ -90,10 +71,7 @@
           @click.stop="handleCloseClick"
         >
           <div>
-            <svg
-              width="10"
-              height="10"
-            >
+            <svg width="10" height="10">
               <path :d="windowIconClose" />
             </svg>
           </div>
@@ -103,18 +81,9 @@
           @click.stop="handleMaximizeClick"
         >
           <div>
-            <svg
-              width="10"
-              height="10"
-            >
-              <path
-                v-show="!isMaximized"
-                :d="windowIconMaximize"
-              />
-              <path
-                v-show="isMaximized"
-                :d="windowIconRestore"
-              />
+            <svg width="10" height="10">
+              <path v-show="!isMaximized" :d="windowIconMaximize" />
+              <path v-show="isMaximized" :d="windowIconRestore" />
             </svg>
           </div>
         </div>
@@ -123,10 +92,7 @@
           @click.stop="handleMinimizeClick"
         >
           <div>
-            <svg
-              width="10"
-              height="10"
-            >
+            <svg width="10" height="10">
               <path :d="windowIconMinimize" />
             </svg>
           </div>
@@ -378,12 +344,16 @@ div.title > span {
   white-space: nowrap;
 }
 
+div.title > span > bdi > span {
+  unicode-bidi: isolate;
+}
+
 .title-bar .title .filename.isOsx:hover {
   color: var(--themeColor);
 }
 
 .active .save-dot {
-  margin-right: 0.25rem;
+  margin-left: 0.25rem;
   width: 8px;
   height: 8px;
   display: inline-block;
